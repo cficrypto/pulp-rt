7910645bed71ce7367611407fece0d1ab6ab1a85
Stefan Tauner <stauner@ecs.tuwien.ac.at>
2021-10-09 05:12:31 +0200

    libs: add support for v* printf functions

---
+++ include/io/stdarg.h
@@ -0,0 +1,19 @@
+#ifndef __RTIO_STDARG_H__
+#define __RTIO_STDARG_H__
+
+#include <stddef.h>
+#include <stdio.h>
+
+// We rely on builtin support
+typedef __builtin_va_list va_list;
+#define va_start(v,l) __builtin_va_start(v,l)
+#define va_end(v) __builtin_va_end(v)
+#define va_arg(v,l) __builtin_va_arg(v,l)
+#define va_copy(d,s) __builtin_va_copy(d,s)
+
+int vprintf(const char *format, va_list ap);
+int vfprintf(FILE *stream, const char *format, va_list ap);
+int vsprintf(char *str, const char *format, va_list ap);
+int vsnprintf(char *str, size_t size, const char *format, va_list ap);
+
+#endif
